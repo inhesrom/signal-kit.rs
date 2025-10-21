0b2d07a989c532bdf06bba41ea0380e74ca03f24
Ian Hersom <ian.hersom@gmail.com>
2025-10-21 17:05:53 -0600

    initial qpsk symbol mapper from random bits

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,20 @@ mod symbol_mapper;
 use random_bit_generator::BitGenerator;
 use symbol_mapper::MapDemap;
 
+use num_complex::Complex32;
+
 fn main()
 {
     let mut bit_gen = BitGenerator::new_from_seed(42);
 
-    // Pull out 2 bits at a time
-    for _ in 0..10 {
+    let mut iq_samples = Vec::<Complex32>::new();
+    let mapper = MapDemap::<f32>::new();
+
+    for _ in 0..80_000_000 {
         let two_bits = bit_gen.next_2_bits();
-        println!("2 bits: {}", two_bits); // 0-3
+        // println!("2 bits: {}", two_bits); // 0-3
+        iq_samples.push(mapper.modulate(two_bits));
+        // println!("{:?}", iq_samples[i]);
     }
 
-    // Pull out 3 bits at a time
-    for _ in 0..10 {
-        let three_bits = bit_gen.next_3_bits();
-        println!("3 bits: {}", three_bits); // 0-7
-    }
-
-    // Single bits
-    for _ in 0..10 {
-        let bit = bit_gen.next_bit();
-        println!("1 bit: {}", bit);
-    }
-
-    // Variable amounts
-    println!("5 bits: {}", bit_gen.next_bits(5)); // 0-31
 }
--- src/symbol_mapper.rs
+++ src/symbol_mapper.rs
@@ -1,31 +1,74 @@
-use bimap::BiHashMap;
+#![allow(dead_code)]
+use std::collections::HashMap;
 use num_complex::Complex;
 use num_traits::Float;
 
 pub struct MapDemap<C> {
-    qpsk: BiHashMap<u8, Complex<C>>,
+    qpsk: HashMap<u8, Complex<C>>,
+    // eight_psk: HashMap<u8, Complex<C>>,
+    // sixteen_apsk: HashMap<u8, Complex<C>>,
 }
 
 impl<C: Float> MapDemap<C> {
-    fn new() -> Self {
-        let sqrt_half = C::from(1.0).unwrap() / C::from(0.5).unwrap().sqrt();
+    pub fn new() -> Self {
+        let mut qpsk = HashMap::new();
+        let one_root_two = C::from(1.0).unwrap() / C::from(2.0).unwrap().sqrt();
+        qpsk.insert(0b00, Complex::new(one_root_two, one_root_two));
+        qpsk.insert(0b01, Complex::new(-one_root_two, one_root_two));
+        qpsk.insert(0b10, Complex::new(one_root_two, -one_root_two));
+        qpsk.insert(0b11, Complex::new(-one_root_two, -one_root_two));
 
-        let mut qpsk = BiHashMap::<u8, Complex<C>>::new();
-        qpsk.insert(0b00, Complex::<C>::new(sqrt_half, sqrt_half));
-        qpsk.insert(0b01, Complex::<C>::new(-sqrt_half, sqrt_half));
-        qpsk.insert(0b10, Complex::<C>::new(sqrt_half, -sqrt_half));
-        qpsk.insert(0b11, Complex::<C>::new(-sqrt_half, -sqrt_half));
-
-        MapDemap { qpsk }
+        MapDemap {
+            qpsk,
+        }
     }
 
     // Map bits to symbol
-    pub fn modulate(&self, bits: u8) -> Option<&Complex<C>> {
-        self.qpsk.get_by_left(&bits)
+    pub fn modulate(&self, bits: u8) -> Complex<C> {
+        self.qpsk.get(&bits).expect("Entered bits must be 11, 00, 10, or 01").clone()
     }
 
-    // Map symbol to bits
-    pub fn demodulate(&self, symbol: &Complex<C>) -> Option<&u8> {
-        self.qpsk.get_by_right(symbol)
+    // Map symbol to bits (finds nearest constellation point)
+    pub fn demodulate(&self, symbol: &Complex<C>) -> Option<u8> {
+        self.qpsk
+            .iter()
+            .min_by(|(_, s1), (_, s2)| {
+                let d1 = (*s1 - *symbol).norm_sqr();
+                let d2 = (*s2 - *symbol).norm_sqr();
+                d1.partial_cmp(&d2).unwrap()
+            })
+            .map(|(bits, _)| *bits)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use num_complex::Complex32;
+
+    use crate::symbol_mapper::MapDemap;
+
+    #[test]
+    fn test_qpsk_mapper() {
+        let zero_zero = 0b00;
+        let zero_one = 0b01;
+        let one_zero = 0b10;
+        let one_one = 0b11;
+
+        let one_root_two = 1.0f32 / 2.0f32.sqrt();
+        let first_quad = Complex32::new(one_root_two, one_root_two);
+        let second_quad = Complex32::new(-one_root_two, one_root_two);
+        let third_quad = Complex32::new(one_root_two, -one_root_two);
+        let fourth_quad = Complex32::new(-one_root_two, -one_root_two);
+
+        let mapper = MapDemap::<f32>::new();
+
+        let iq = mapper.modulate(zero_zero);
+        assert_eq!(iq, first_quad);
+        let iq = mapper.modulate(zero_one);
+        assert_eq!(iq, second_quad);
+        let iq = mapper.modulate(one_zero);
+        assert_eq!(iq, third_quad);
+        let iq = mapper.modulate(one_one);
+        assert_eq!(iq, fourth_quad);
     }
 }
