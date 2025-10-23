00ce688d79c58c3dc9c31515a909915d538918f2
Ian Hersom <ian.hersom@gmail.com>
2025-10-22 22:13:12 -0600

    updating rrc

--- src/rrc_filter.rs
+++ src/rrc_filter.rs
@@ -1,4 +1,6 @@
-pub struct RRCFilter<V> {
+use std::f64::consts::PI;
+
+pub struct RRCFilter {
     num_filter_taps: usize,
     sample_rate: f64,
     symbol_rate: f64,
@@ -24,12 +26,25 @@ impl<V> RRCFilter<V> {
     }
 
     pub fn build_filter(&self) -> V {
-        let mut h: V;
+        let mut h = V::new();
         for i in 0..self.num_filter_taps {
             let t: f64 = (i as f64 - (num_filter_taps as f64) / 2.0) * self.scale;
-            if t == 0.0 {
 
-            } else if t.abs() == (self.T) {}
+            if t == 0.0 {
+                h.push((1.0 + self.beta * (4.0/PI - 1.0)) / self.T);
+            }
+            else if t.abs() == self.T / (4.0 * self.beta) {
+                h.push(
+                    (self.beta/2.0.sqrt()) * ((1.0 + 2.0/PI) * (PI / (4.0*self.beta)).sin()) +
+                       (1.0-2.0/PI) * (PI/(4.0*self.beta)).cos() / self.T
+                );
+            }
+            else {
+                h.push(
+                    ((PI*t*(1.0-self.beta)/self.T).sin()) + 4.0*self.beta*t*(PI*t*(1.0+self.beta)/self.T).cos()/self.T) /
+                    (PI*t*(1.0-(4.0*self.beta*t/self.T)*(4.0*self.beta*t/self.T))/self.T)
+                );
+            }
         }
     }
 }
